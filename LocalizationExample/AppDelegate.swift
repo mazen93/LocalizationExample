36d7f9d17b2b31da8d6d1615b52b5551e18ec496
7amody <mohamed_mazin10@hotmail.com>
2019-06-19 12:24:55 +0200

    reset func adedd

--- LocalizationExample/AppDelegate.swift
+++ LocalizationExample/AppDelegate.swift
@@ -10,14 +10,7 @@ import UIKit
 import MOLH
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate,MOLHResetable {
-    func reset() {
-        let rootviewcontroller: UIWindow = ((UIApplication.shared.delegate?.window)!)!
-        let stry = UIStoryboard(name: "Main", bundle: nil)
-        rootviewcontroller.rootViewController = stry.instantiateViewController(withIdentifier: "ViewController")
-        
-        
-        
-    }
+   
     
 
     var window: UIWindow?
@@ -28,7 +21,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate,MOLHResetable {
         MOLH.shared.activate(true)
         return true
     }
-
+    func reset() {
+        let rootviewcontroller: UIWindow = ((UIApplication.shared.delegate?.window)!)!
+        let stry = UIStoryboard(name: "Main", bundle: nil)
+        rootviewcontroller.rootViewController = stry.instantiateViewController(withIdentifier: "ViewController") 
+        
+    }
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
